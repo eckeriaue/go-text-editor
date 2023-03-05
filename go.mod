ee1165165bd27a563b34f0166955d16b5897bfb7
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2023-03-05 17:31:23 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module editor
+
+go 1.20
+
+require fyne.io/fyne/v2 v2.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bebra string = "bebra string"
+	fmt.Printf("%s", bebra)
+}
